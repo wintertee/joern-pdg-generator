a811da4a6659ee829882cb5c76533a7aaf9dc861
wintertee <27325188+wintertee@users.noreply.github.com>
2025-05-22 11:38:02 +0800

    feat: add operator_method_body_node

--- src/predicates.py
+++ src/predicates.py
@@ -20,8 +20,38 @@ def ast_leaves_node(node, data, graph) -> bool:
     """
     Some AST leafs are not useful for the analysis, we remove them
     """
-    AST_LEAFS = ["IDENTIFIER", "LITERAL", "FIELD_IDENTIFIER"]
+    AST_LEAFS = [
+        "IDENTIFIER",
+        "LITERAL",
+        "FIELD_IDENTIFIER",
+        "LOCAL",
+        "MEMBER",
+        "MODIFIER",
+    ]
     if data["label"] in AST_LEAFS:
         return True
+
+    return False
+
+
+def operator_method_body_node(node, data, graph) -> bool:
+    """
+    We remove methods not explicitly defined in the code.
+    """
+
+
+    def is_operator_root(data):
+        return data["label"] == "METHOD" and "LINE_NUMBER" not in data.keys()
+
+    if is_operator_root(data):
+        # return True
+        pass
+
+    else:
+        for u, v, k, data in graph.in_edges(node, keys=True, data=True):
+            if data["label"] == "AST":
+                parent_data = graph.nodes[u]
+                if is_operator_root(parent_data):
+                    return True
 
     return False
